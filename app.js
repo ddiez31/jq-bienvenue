8c94e67ad5ae1ccc2fbde73a0eecfe4bb5ddcb62
David <david.diez31@free.fr>
2017-01-05 14:25:33 +0100

    exercice ok

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('have fun !');
+$(document).ready(function() {
+
+    console.log('have fun !');
+
+    $("input").focusout(function() {
+
+        var firstName = $("#first_name").val();
+        var lastName = $("#last_name").val();
+        var city = $("#city").val();
+        console.log(firstName);
+        console.log(lastName);
+        console.log(city);
+        if (firstName == "" || lastName == "") {
+            $('body').append($('<div/>').html = "Veuillez renseigner tous les champs");
+
+        } else {
+            $("span#username").html(" " + firstName + " " + lastName);
+        }
+    });
+});
